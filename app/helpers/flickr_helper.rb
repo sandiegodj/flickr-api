4b2f568b4afc46d7704cea6be36f36435b2ba113
sandiegodj <davidjanczyn@gmail.com>
2014-10-15 15:31:20 -0700

    limit number of photos

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -3,10 +3,10 @@ module FlickrHelper
 
 
   def user_photos(user_id, photo_count = 12)
-    flickr.photos.search(:user_id => user_id)
+    flickr.photos.search(:user_id => user_id).to_a[0..(photo_count - 1)]
   end
   
-  def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
+  def render_flickr_sidebar_widget(user_id, photo_count = 12)
     begin
       photos = user_photos(user_id, photo_count)
       render :partial => '/flickr/display', :locals => { :photos => photos }
